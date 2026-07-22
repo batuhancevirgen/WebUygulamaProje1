98cfc6001f0928221c72b7a75d491abeb44b914f
batuhancevirgen <batuhan.cevirgen@ogr.sakarya.edu.tr>
2026-07-22 20:04:28 +0300

    Initial commit : proje altyapısı ve DbContext kurulumu yapıldı

---
+++ Models/KitapTuru.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUygulamaProje1.Models
+{
+    public class KitapTuru
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+
+        [Required] //not null
+        public string Ad { get; set; }
+    }
+}
---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) 
+        {
+               
+        }
+
+    }
+}
