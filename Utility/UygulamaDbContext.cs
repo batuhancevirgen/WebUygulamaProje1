d47adb22a51b8fe5bbb06d52966638646573eb83
batuhancevirgen <batuhan.cevirgen@ogr.sakarya.edu.tr>
2026-07-24 17:13:05 +0300

    KitapTuru Controller DI yapisi kuruldu ve veriler View'a gonderildi

--- Utility/UygulamaDbContext.cs
+++ Utility/UygulamaDbContext.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebUygulamaProje1.Models;
 
 namespace WebUygulamaProje1.Utility
 {
     public class UygulamaDbContext : DbContext
     {
-        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) 
-        {
-               
-        }
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) { }
+
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
 
     }
 }
