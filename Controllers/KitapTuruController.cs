0d8c64eb6114e63c6581ab30f4fdfb208fb5c376
batuhancevirgen <batuhan.cevirgen@ogr.sakarya.edu.tr>
2026-07-25 01:28:17 +0300

    CRUD islemleri (Update, Delete) tamamlandi ve TempData bildirim yapisi eklendi

--- Controllers/KitapTuruController.cs
+++ Controllers/KitapTuruController.cs
@@ -29,9 +29,63 @@ namespace WebUygulamaProje1.Controllers
             {
                 _uygulamaDbContext.KitapTurleri.Add(kitapTuru);
                 _uygulamaDbContext.SaveChanges();
+                TempData["basarili"] = "Yeni Kitap Türü Başarıyla Oluşturuldu";
                 return RedirectToAction("Index", "KitapTuru");
             }
             return View();
-        }   
+        }
+        
+        public IActionResult Guncelle(int? id)
+        {   
+            if(id == null || id==0)
+            {
+                return NotFound();
+            }
+            KitapTuru? kitapTuruVt = _uygulamaDbContext.KitapTurleri.Find(id);
+            if (kitapTuruVt == null) { return NotFound(); }
+            return View(kitapTuruVt); //neyi güncelleneceğini belirtmek için
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(KitapTuru kitapTuru)
+        {
+            if (ModelState.IsValid)
+            {
+                _uygulamaDbContext.KitapTurleri.Update(kitapTuru);
+                _uygulamaDbContext.SaveChanges();
+                TempData["basarili"] = "Kitap Türü Başarıyla Güncellendi ";
+                return RedirectToAction("Index", "KitapTuru");
+            }
+            return View();
+        }
+
+        public IActionResult Sil(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            KitapTuru? kitapTuruVt = _uygulamaDbContext.KitapTurleri.Find(id);
+            if (kitapTuruVt == null) { return NotFound(); }
+            return View(kitapTuruVt); //neyi güncelleneceğini belirtmek için
+        }
+
+        [HttpPost,ActionName("Sil")]
+        public IActionResult SilPOST(int? id)
+        {
+            KitapTuru? kitapTuru = _uygulamaDbContext.KitapTurleri.Find(id);
+
+            if(kitapTuru == null)
+            {
+                return NotFound();
+            }
+            _uygulamaDbContext.KitapTurleri.Remove(kitapTuru);
+            _uygulamaDbContext.SaveChanges();
+            TempData["basarili"] = "Kitap Türü Başarıyla Silindi";
+            return RedirectToAction("Index", "KitapTuru");
+
+        }
+
+
     }
 }
